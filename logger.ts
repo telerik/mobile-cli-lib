1422f0d5a432c2905229bbe205580b9c739792ac
Fatme <hfatme@gmail.com>
2014-07-01 15:06:56 +0300

    Add basic common classes

---
+++ logger.ts
@@ -0,0 +1,69 @@
+///<reference path="../.d.ts"/>
+
+import log4js = require("log4js");
+import util = require("util");
+
+export class Logger implements ILogger {
+	private log4jsLogger = null;
+	
+	public setLoggerConfiguration(config: any, logLevel: string): void {
+		var appenders = [];
+
+		if (!config.CI_LOGGER) {
+			appenders.push({
+				type: "console",
+				layout: {
+					type: "messagePassThrough"
+				}
+			});
+		}
+
+		log4js.configure({appenders: appenders});
+
+		this.log4jsLogger = log4js.getLogger();
+
+		if (logLevel) {
+			this.log4jsLogger.setLevel(logLevel);
+		} else {
+			this.log4jsLogger.setLevel(config.DEBUG ? "TRACE" : "INFO");
+		}
+	}
+
+	setLevel(level: string): void {
+		this.log4jsLogger.setLevel(level);
+	}
+
+	fatal(...args): void {
+		this.log4jsLogger.fatal.apply(this.log4jsLogger, args);
+	}
+
+	error(...args): void {
+		this.log4jsLogger.error.apply(this.log4jsLogger, args);
+	}
+
+	warn(...args): void {
+		this.log4jsLogger.warn.apply(this.log4jsLogger, args);
+	}
+
+	info(...args): void {
+		this.log4jsLogger.info.apply(this.log4jsLogger, args);
+	}
+
+	debug(...args): void {
+		this.log4jsLogger.debug.apply(this.log4jsLogger, args);
+	}
+
+	trace(...args): void {
+		this.log4jsLogger.trace.apply(this.log4jsLogger, args);
+	}
+
+	out(...args): void {
+		console.log(util.format.apply(null, args));
+	}
+
+	write(...args): void {
+		process.stdout.write(util.format.apply(null, args));
+	}
+}
+$injector.register("logger", Logger);
+
