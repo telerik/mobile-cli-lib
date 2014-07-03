a4294787f9ce49b5de608ed02cfc08124594ab32
Fatme <hfatme@gmail.com>
2014-07-03 09:46:59 +0300

    More common definitions files

---
+++ declarations.d.ts
@@ -0,0 +1,78 @@
+interface IDisposable {
+	dispose(): void;
+}
+
+interface IFileSystem {
+	zipFiles(zipFile: string, files: string[], zipPathCallback: (path: string) => string): IFuture<void>;
+	unzip(zipFile: string, destinationDir: string): IFuture<void>;
+	exists(path: string): IFuture<boolean>;
+	deleteFile(path: string): IFuture<void>;
+	deleteDirectory(directory: string): IFuture<void>;
+	getFileSize(path: string): IFuture<number>;
+	futureFromEvent(eventEmitter: any, event: string): IFuture<any>;
+	createDirectory(path: string): IFuture<void>;
+	readDirectory(path: string): IFuture<string[]>;
+	readFile(filename: string): IFuture<NodeBuffer>;
+	readText(filename: string, encoding?: string): IFuture<string>;
+	readJson(filename: string, encoding?: string): IFuture<any>;
+	writeFile(filename: string, data: any, encoding?: string): IFuture<void>;
+	writeJson(filename: string, data: any, space?: string, encoding?: string): IFuture<void>;
+	copyFile(sourceFileName: string, destinationFileName: string): IFuture<void>;
+	getUniqueFileName(baseName: string): IFuture<string>;
+	getFsStats(path: string): IFuture<IFsStats>;
+
+	createReadStream(path: string, options?: {
+		flags?: string;
+		encoding?: string;
+		fd?: string;
+		mode?: number;
+		bufferSize?: number;
+	}): any;
+	createWriteStream(path: string, options?: {
+		flags?: string;
+		encoding?: string;
+		string?: string;
+	}): any;
+
+	chmod(path: string, mode: number): IFuture<any>;
+}
+
+// duplicated from fs.Stats, because I cannot import it here
+interface IFsStats {
+	isFile(): boolean;
+	isDirectory(): boolean;
+	isBlockDevice(): boolean;
+	isCharacterDevice(): boolean;
+	isSymbolicLink(): boolean;
+	isFIFO(): boolean;
+	isSocket(): boolean;
+	dev: number;
+	ino: number;
+	mode: number;
+	nlink: number;
+	uid: number;
+	gid: number;
+	rdev: number;
+	size: number;
+	blksize: number;
+	blocks: number;
+	atime: Date;
+	mtime: Date;
+	ctime: Date;
+}
+
+interface IErrors {
+	fail(formatStr: string, ...args: any[]): void;
+	fail(opts: {formatStr?: string; errorCode?: number; suppressCommandHelp?: boolean}, ...args: any[]): void;
+
+	beginCommand(action: () => any, printCommandHelp: () => void): any;
+	verifyHeap(message: string): void;
+}
+
+interface ICommandOptions {
+	disableAnalytics?: boolean;
+}
+
+declare enum ErrorCodes {
+	UNKNOWN = 127
+}
