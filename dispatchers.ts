cc2287c290959b3564ff13b3a164bba499ce5002
rosen-vladimirov <rosen.vladimirov@telerik.com>
2014-10-29 14:27:44 +0200

    Remove fibers leaks
    Remove the intentional leak of fibers. It was used to workaround bug in node-fibers which is already fixed, so we do not need the workaround anymore.
    http://teampulse.telerik.com/view#item/277114

--- dispatchers.ts
+++ dispatchers.ts
@@ -98,7 +98,7 @@ class FutureDispatcher implements IFutureDispatcher {
 			$injector.dispose();
 			Future.assertNoFutureLeftBehind();
 		});
-		global.__main_fiber__ = fiber; // leak fiber to prevent it from being GC'd and thus corrupting V8
+
 		fiber.run();
     }
 }
