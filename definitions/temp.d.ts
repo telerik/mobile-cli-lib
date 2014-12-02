500dd787fd9ee7616c51a645843e108b98290eb4
rosen-vladimirov <rosen.vladimirov@telerik.com>
2014-12-02 10:14:29 +0200

    Add start and end for createReadableStream
    Add start and end options for createReadableStream.

--- definitions/temp.d.ts
+++ definitions/temp.d.ts
@@ -4,9 +4,15 @@ interface ITempPathOptions {
 	suffix?: string;
 }
 
+interface ITempFileInfo {
+	path?: string;
+	fd?: any;
+}
+
 declare module "temp" {
 	function track(): void;
 	function cleanup(): void;
 	function mkdirSync(affixes: string): string;
-	function path(options: ITempPathOptions) : string;
+	function path(options: ITempPathOptions): string;
+	function open(name: string, fn: (error: Error, info: ITempFileInfo) => void): any;
 }
