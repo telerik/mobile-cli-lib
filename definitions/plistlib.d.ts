782f2adb6518f12bc9eb4b3df625f2498beaf781
Panayot Cankov <Panayot.Cankov@telerik.com>
2015-02-02 18:21:16 +0200

    Add ios debugging
    Implement device port forwarding

--- definitions/plistlib.d.ts
+++ definitions/plistlib.d.ts
@@ -1,3 +1,5 @@
 declare module "plistlib" {
-	export function toString(data: any): string;
+    export function toString(data: any): string;
+    export function loadBuffer(data: NodeBuffer, callback: (err: any, plist: any) => void): void;
+    export function loadString(data: string, callback: (err: any, plist: any) => void): void;
 }
