6fe4ef7eb3aa5d9bca244ca990303c04bd9fb298
rosen-vladimirov <rosen.vladimirov@telerik.com>
2016-02-29 10:32:37 +0200

    Fix sync of platform specific and config specific files
    Fix LiveSync of platform specific and configuration specific files by extracting logic for parsing filename and use it in NS CLI.
    Add ProjectFilesProviderBase class that holds the logic, so it can still be used from ProjectFilesManager.
    
    Add --release option to common options so when `tns livesync <platform> --release --watch` is used, the files will be processed correctly.
    Add tests for the new ProjectFilesProviderBase class.

---
+++ services/project-files-provider-base.ts
@@ -0,0 +1,45 @@
+///<reference path="../.d.ts"/>
+"use strict";
+
+import * as path from "path";
+import * as util from "util";
+
+export abstract class ProjectFilesProviderBase implements IProjectFilesProvider {
+	abstract isFileExcluded(filePath: string): boolean;
+	abstract mapFilePath(filePath: string, platform: string): string;
+
+	constructor(private $mobileHelper: Mobile.IMobileHelper,
+		private $options: ICommonOptions) {}
+
+	public getPreparedFilePath(filePath: string): string {
+		let projectFileInfo = this.getProjectFileInfo(filePath);
+		return path.join(path.dirname(filePath), projectFileInfo.onDeviceFileName);
+	}
+
+	public getProjectFileInfo(filePath: string, platform?: string): IProjectFileInfo {
+		let parsed = this.parseFile(filePath, this.$mobileHelper.platformNames, platform || "");
+		if (!parsed) {
+			parsed = this.parseFile(filePath, ["debug", "release"], this.$options.release ? "release" : "debug");
+		}
+
+		return parsed || {
+			filePath: filePath,
+			onDeviceFileName: path.basename(filePath),
+			shouldIncludeFile: true
+		};
+	}
+
+	private parseFile(filePath: string, validValues: string[], value: string): IProjectFileInfo {
+		let regex = util.format("^(.+?)[.](%s)([.].+?)$", validValues.join("|"));
+		let parsed = filePath.match(new RegExp(regex, "i"));
+		if (parsed) {
+			return {
+				filePath: filePath,
+				onDeviceFileName: path.basename(parsed[1] + parsed[3]),
+				shouldIncludeFile: parsed[2].toLowerCase() === value.toLowerCase()
+			};
+		}
+
+		return null;
+	}
+}
