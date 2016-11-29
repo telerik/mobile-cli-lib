e6565f41386972d9d4b4cc933e2179d330644a3d
rosen-vladimirov <rosen.vladimirov@telerik.com>
2016-11-29 13:35:54 +0200

    Break when Node.js is not supported
    CLI will not be able to work with any Node.js version below 4.2.1. So in case user's version is older, stop the process and inform the user that he needs newer version.

--- verify-node-version.ts
+++ verify-node-version.ts
@@ -1,4 +1,3 @@
-
 // This function must be separate to avoid dependencies on C++ modules - it must execute precisely when other functions cannot
 
 import {EOL} from "os";
@@ -6,24 +5,21 @@ import * as semver from "semver";
 
 // These versions cannot be used with CLI due to bugs in the node itself.
 // We are absolutely sure we cannot work with them, so inform the user if he is trying to use any of them and exit the process.
-let versionsCausingFailure = ["0.10.34", "4.0.0", "4.2.0", "5.0.0"];
+const versionsCausingFailure = ["0.10.34", "4.0.0", "4.2.0", "5.0.0"];
+const minimumRequiredVersion = "4.2.1";
 
 export function verifyNodeVersion(supportedVersionsRange: string, cliName: string, deprecationVersion: string): void {
 	// The colors module should not be assigned to variable because the lint task will fail for not used variable.
 	require("colors");
-	let nodeVer = process.version.substr(1);
+	const nodeVer = process.version.substr(1);
 
-	if (versionsCausingFailure.indexOf(nodeVer) !== -1) {
+	if (versionsCausingFailure.indexOf(nodeVer) !== -1 || !semver.valid(nodeVer) || semver.lt(nodeVer, minimumRequiredVersion)) {
 		console.error(`${EOL}Node.js '${nodeVer}' is not supported. To be able to work with ${cliName} CLI, install any Node.js version in the following range: ${supportedVersionsRange}.${EOL}`.red.bold);
 		process.exit(1);
 	}
 
-	if (semver.satisfies(nodeVer, "~0.12.0")) {
-		console.warn(`${EOL}Support for Node.js 0.12.x is deprecated and will be removed in the ${cliName} ${deprecationVersion} release. Please, upgrade to the latest Node.js LTS version.${EOL}`.yellow.bold);
-	} else {
-		let checkSatisfied = semver.satisfies(nodeVer, supportedVersionsRange);
-		if (!checkSatisfied) {
-			console.log(`${EOL}Support for Node.js ${nodeVer} is not verified. This CLI might not install or run properly.${EOL}`.yellow.bold);
-		}
+	const checkSatisfied = semver.satisfies(nodeVer, supportedVersionsRange);
+	if (!checkSatisfied) {
+		console.log(`${EOL}Support for Node.js ${nodeVer} is not verified. This CLI might not install or run properly.${EOL}`.yellow.bold);
 	}
 }
