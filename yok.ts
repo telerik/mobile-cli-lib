9a011767c7450ff083d940a0fc05c963a9609a3e
Tsvetan Milanov <TsvetanMilanov@users.noreply.github.com>
2017-01-20 11:59:25 +0200

    Change the resolve constructor logic in yok

--- yok.ts
+++ yok.ts
@@ -361,8 +361,7 @@ export class Yok implements IInjector {
 			}
 		});
 
-		let name = ctor.$inject.name;
-		if (name && name[0] === name[0].toUpperCase()) {
+		if (ctor.constructor && ctor.prototype && ctor.prototype.constructor) {
 			return new (<any>ctor)(...resolvedArgs);
 		} else {
 			return ctor.apply(null, resolvedArgs);
