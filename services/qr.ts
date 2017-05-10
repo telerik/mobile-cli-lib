ebfaff5199c33becd1bb350306f4fbdbc0204906
Dimitar Kerezov <dimitar.kerezov@telerik.com>
2017-05-10 17:38:10 +0300

    Use qr-image for qr code generation

--- services/qr.ts
+++ services/qr.ts
@@ -1,56 +1,20 @@
-let qrcode = require("qrcode-generator");
+import { imageSync } from "qr-image";
+import { escape } from "querystring";
 
 export class QrCodeGenerator implements IQrCodeGenerator {
-	// The order is important.
-	private static ERROR_CORRECTION_LEVEL = ["L", "M", "Q", "H"];
-
-	// https://en.wikiversity.org/wiki/Reed%E2%80%93Solomon_codes_for_coders/Additional_information
-	private static MAX_BLOCK_VERSION = 40;
-
-	constructor(private $staticConfig: Config.IStaticConfig) { }
-
-	public async generateQrCode(data: string): Promise<any> {
-		let errorCorrectionLevel = "L";
-		let errorCorrectionOffset = _.indexOf(QrCodeGenerator.ERROR_CORRECTION_LEVEL, errorCorrectionLevel);
-
-		// 4 is from the qrcode-generator source code.
-		let maxReedSolomonBlockIndex = QrCodeGenerator.MAX_BLOCK_VERSION / 4 - errorCorrectionOffset;
-
-		for (let i = 1; i <= maxReedSolomonBlockIndex; ++i) {
-			let qr = qrcode(i, errorCorrectionLevel);
-			try {
-				qr.addData(data);
-				qr.make();
-			} catch (ex) {
-				let expected = "code length overflow.";
-				if (ex.message && ex.message.substr(0, expected.length) === expected) {
-					continue;
-				} else {
-					throw ex;
-				}
-			}
-
-			return qr;
-		}
-
-		// Since the max Reed-Solomon block index was calculated before the for loop and no exception was thrown in it here the only error can be because of long project name.
-		// Return null and expect the consumer to take caution in handling this case
-		return null;
-	}
+	constructor(private $staticConfig: Config.IStaticConfig,
+		private $logger: ILogger) { }
 
 	public async generateDataUri(data: string): Promise<string> {
-		let qr = await this.generateQrCode(data);
-		let dataUri: string = null;
-		if (qr) {
-			let cells = qr.getModuleCount();
-			let size = this.$staticConfig.QR_SIZE;
-			let cellSize = Math.ceil(size / (cells + 2 * 4 /* margin */));
-
-			let imgTag = qr.createImgTag(cellSize);
-			dataUri = imgTag.split('src="')[1].split('"')[0];
+		let result: string = null;
+		try {
+			const qrSvg = imageSync(data, { size: this.$staticConfig.QR_SIZE, type: "svg" }).toString();
+			result = `data:image/svg+xml;utf-8,${escape(qrSvg)}`;
+		} catch (err) {
+			this.$logger.trace(`Failed to generate QR code for ${data}`, err);
 		}
 
-		return dataUri;
+		return result;
 	}
 }
 
